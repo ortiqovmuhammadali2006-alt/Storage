0f2fe81d89aae10c08757ac52c5ab363989eba5a
Muhammadali <ortiqovmuhammadali2006@gmail.com>
2026-03-31 17:19:41 +0500

    add_to_cart qismi tayyorlandi

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import home, product_by_category, product_detail, cart_view, category_detail
+from .views import home, product_by_category, product_detail, cart_view, category_detail, add_to_cart
 
 urlpatterns = [
     path('', home, name='home'), 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('product-detail/<slug:slug>/', product_detail, name='detail'),
     path('category-detail/<slug:slug>/', category_detail, name='category_detail'),
     path('cart/', cart_view, name='cart'),
+    path('add-to-cart/<slug:product_slug>/', add_to_cart, name='add_to_cart'),
     
 ]
